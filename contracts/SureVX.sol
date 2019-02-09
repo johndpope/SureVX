e11e0dbbe90953cff7f957e20452e18736d5b6f7
Radial Manner <ben@radialmanner.com>
2019-02-09 12:45:48 +0100

    Importing SafeMath

--- contracts/SureVX.sol
+++ contracts/SureVX.sol
@@ -1,26 +1,31 @@
 pragma solidity ^0.5.1;
 
-// Import statements here
-// Import statements here
+import 'SafeMath.sol';
 
-/**
-  * @title
-  * @author Radial Manner
-  * @notice Factory contract creating a simple collateral position
-  *         between two counterparties.
- */
+/// @title
+/// @author Radial Manner
+/// @notice Factory contract creating a simple collateral position between two counterparties.
 
 contract CollatFactory {
-    address[] public collatAddresses;
+    address[] private collatAddresses;
+    mapping (address => address) private collatCPTY1;
     
-    function createCollat() public {
+    function createCollat() public returns(address) {
         Collat c = new Collat();
         collatAddresses.push(address(c));
+        collatCPTY1[msg.sender] = address(c);
+        return address(c);
     }
     
     function getCollatCount() public view returns(uint collatCount) {
         return collatAddresses.length;
     }
+    
+    /// @return array of addresses of ALL Collats
+    function getCollatAddresses() public view returns(address[] memory) {
+        return collatAddresses;
+    }
+    
 }
 
 contract Collat { 
