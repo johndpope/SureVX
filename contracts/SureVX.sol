4963904461ab1ecaefd0cf981466f1f9e7124063
Radial Manner <ben@radialmanner.com>
2019-02-10 13:21:10 +0100

    Basic functionality

--- contracts/SureVX.sol
+++ contracts/SureVX.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.1;
 
-import 'SafeMath.sol';
+//import 'SafeMath.sol';
+//import 'Ownable.sol';
 
-/// @title
+/// @title SureVX
 /// @author Radial Manner
 /// @notice Factory contract creating a simple collateral position between two counterparties.
 
@@ -10,13 +11,16 @@ contract CollatFactory {
     address[] private collatAddresses;
     mapping (address => address) private collatCPTY1;
     
-    function createCollat() public returns(address) {
-        Collat c = new Collat();
+    /// @return address of newly created Collat
+    function createCollat(address _cpty2, uint _amount) public returns(address) {
+        address cpty1 = msg.sender;
+        Collat c = new Collat(cpty1, _cpty2, _amount);
         collatAddresses.push(address(c));
         collatCPTY1[msg.sender] = address(c);
         return address(c);
     }
     
+    /// @return uint - number of collats
     function getCollatCount() public view returns(uint collatCount) {
         return collatAddresses.length;
     }
@@ -30,10 +34,17 @@ contract CollatFactory {
 
 contract Collat { 
     
-    // Variables here
-    // At this point I'm just testing Github access from the desktop app
+        address public cpty1;
+        address public cpty2;
+        uint public amount;
+
+    constructor (address _cpty1, address _cpty2, uint _amount) public {
+            cpty1 = _cpty1;
+            cpty2 = _cpty2;
+            amount = _amount;
+    }
     
-    constructor () public {
-        // TODO
+    function getCollatInfo() public view returns (address, address, uint) {
+        return (cpty1, cpty2, amount);
     }
 }
